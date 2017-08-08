2ed3b696a313cd56f8e0e8105468861d834ee880
miyi <mylegojesus@gmail.com>
2017-08-08 04:50:21 -0700

    hovermarker generated from state array

--- src/data/fakedata.js
+++ src/data/fakedata.js
@@ -9,9 +9,9 @@ export const markerData = [...Array(TOTAL_COUNT)].fill(0) // fill(0) for loose m
     lat: UBCCoords.lat +
       (0.2 * (index + 1) *
       Math.sin(30 * Math.PI * 4 * index / 180) *
-      Math.cos(50 * Math.PI * 4 * index / 180) + Math.sin(5 * 4 * index / 180)) / 200,
+      Math.cos(50 * Math.PI * 4 * index / 180) + Math.sin(5 * 4 * index / 180)) / 250,
     lng: UBCCoords.lng +
       (0.2 * (index + 1) *
       Math.cos(70 + 23 * Math.PI * 4 * index / 180) *
-      Math.cos(50 * Math.PI * 4 * index / 180) + Math.sin(5 * 4 * index / 180)) / 200,
+      Math.cos(50 * Math.PI * 4 * index / 180) + Math.sin(5 * 4 * index / 180)) / 250,
   }));
